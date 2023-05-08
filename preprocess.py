8124ef7e39e870e122487b80caf928ea0fcb9232
lior motola <lior.motola@campus.technion.ac.il>
2023-05-08 17:53:27 +0300

    utils and preprocess

---
+++ preprocess.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import os
+from tqdm import tqdm
+
+def create_patient_df(psv_file):
+    df = pd.read_csv(psv_file, delimiter="|")
+    patient_id = psv_file.split("patient_")[-1].strip(".psv")
+    df["patient_id"] = [patient_id] * len(df)
+    df["match"] = df.SepsisLabel != df.SepsisLabel.shift()
+    if len(df[df["match"]]) == 2:
+        shift_index = df[df["match"]].index[-1]
+        df = df[df.index <= shift_index]
+    elif df["SepsisLabel"][0] ==1:
+        df = df[df.index <= 0]
+
+    df.drop(columns=["match"], inplace=True)
+    return df
+
+def create_data(dir_path,csv_name):
+    patient_files = os.listdir(dir_path)
+    first_patient = os.path.join(dir_path,patient_files[0])
+    final_df = create_patient_df(first_patient)
+    for patient in tqdm(patient_files[1:]):
+        patient_file = os.path.join(dir_path,patient)
+        p_df = create_patient_df(patient_file)
+        final_df = pd.concat([final_df,p_df])
+
+    final_df.to_csv(csv_name, index=False)
+    return final_df
+
+def preprocess_train_data(csv_file, keep_cols: list):
+    df = pd.read_csv(csv_file)
+
+    # dropping irrelevant cols
+    drop_cols = list(set(df.columns).difference(set(keep_cols)))
+    dropped_df = df.drop(columns=drop_cols)
+    dropped_df['age_group'] = pd.cut(dropped_df['Age'], bins=[0, 18, 30, 50, 70, 90, 100], labels=[0, 1, 2, 3, 4, 5])
+
+    # imputing null values
+    imputed_df = dropped_df.copy()
+    patients_grouped_df = imputed_df.groupby('patient_id')
+    # ffil= forward fill = last observation carried forward
+    # bfill = backward fill = if there are no previous records so we will take from later records
+    imputed_df= patients_grouped_df.apply(lambda row: row.ffill().bfill())
+    df = imputed_df.copy()
+    grouped_df = df.groupby(["age_group", "Gender"])
+    cols = list(set(df.columns).difference({"age_group", "Gender", "SepsisLabel"}))
+    df[cols] = grouped_df[cols].transform(lambda x: x.fillna(x.mean()))
+    df = df.drop("patient_id", axis=1).reset_index().drop("level_1",axis=1)
+
+    return df , grouped_df.mean()
+
+def preprocess_test(csv_file, keep_cols: list,fill_null_vals:dict):
+    df = pd.read_csv(csv_file)
+
+    # dropping irrelevant cols
+    drop_cols = list(set(df.columns).difference(set(keep_cols)))
+    dropped_df = df.drop(columns=drop_cols)
+    dropped_df['age_group'] = pd.cut(dropped_df['Age'], bins=[0, 18, 30, 50, 70, 90, 100], labels=[0, 1, 2, 3, 4, 5])
+
+    # imputing null values
+    imputed_df = dropped_df.copy()
+    patients_grouped_df = imputed_df.groupby('patient_id')
+    # ffil= forward fill = last observation carried forward
+    # bfill = backward fill = if there are no previous records so we will take from later records
+    imputed_df= patients_grouped_df.apply(lambda row: row.ffill().bfill())
+
+    for index, values in fill_null_vals.items():
+        age, gender = index
+        imputed_df[(imputed_df.age_group == age) & (imputed_df.Gender == gender)] = imputed_df[
+            (imputed_df.age_group == age) & (imputed_df.Gender == gender)].fillna(value=values)
+    imputed_df = imputed_df.drop("patient_id", axis=1).reset_index().drop("level_1", axis=1)
+
+    return imputed_df
+
+if __name__ == '__main__':
+    dir = "data/test"
+    # df = pd.read_csv(os.path.join(dir,"patient_16068.psv"), delimiter="|")
+    create_data(dir,csv_name="test_data_merged_final.csv")
---
+++ utils.py
@@ -0,0 +1,90 @@
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import confusion_matrix, mean_squared_error
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+
+def weighted_avrage(group):
+    n = len(group) + 1
+    return group[:-1].sum() / n + 2 * group.tail(1) / n
+
+
+def aggregate_df_weighted_mean(stat_cols, dynamic_cols, initial_df):
+    grouped_by_patient_df = initial_df.groupby("patient_id")
+    aggregated_df_stat = grouped_by_patient_df[stat_cols].max()
+    aggregated_df_dynamic = grouped_by_patient_df[dynamic_cols].apply(weighted_avrage).reset_index(level=1)
+    final_aggregated_df = aggregated_df_stat.join(aggregated_df_dynamic, on="patient_id")
+
+    return final_aggregated_df
+
+def last_n_rows(group, n=10):
+    return group.tail(n)
+
+
+def aggregate_df_reg_mean(stat_cols, dynamic_cols, initial_df):
+
+    grouped_by_patient_df = initial_df.groupby("patient_id")
+    aggregated_df_stat = grouped_by_patient_df[stat_cols].max()
+    aggregated_df_dynamic = grouped_by_patient_df[dynamic_cols].mean()
+    final_aggregated_df = aggregated_df_stat.join(aggregated_df_dynamic, on="patient_id")
+
+    return final_aggregated_df
+
+def model_eval(true_labels, predicted_labels,set_type : str,title:str):
+    acc= accuracy_score(true_labels, predicted_labels)
+    print(f"{set_type} Accuracy: ", acc)
+    precision = precision_score(true_labels, predicted_labels)
+    print(f"{set_type} Precision: ", precision)
+    recall = recall_score(true_labels, predicted_labels)
+    print(f"{set_type} Recall: ", recall)
+    f1 = f1_score(true_labels, predicted_labels)
+    print(f"{set_type} F1 Score: ", f1)
+    rmse = np.sqrt(mean_squared_error(true_labels, predicted_labels))
+    print(f"{set_type} RMSE: ", rmse)
+    conf_mat = confusion_matrix(true_labels, predicted_labels)
+    sns.heatmap(conf_mat, annot=True, fmt='d')
+    plt.ylabel("True Values")
+    plt.xlabel("Predicted Values")
+    plt.title(f"{title}-{set_type}")
+    plt.savefig(f"{title} {set_type}-confusion matrix")
+    plt.show()
+
+def ICULOS_analysis(predictions_df):
+    levels = [(0, 30), (30, 55), (55, 400)]
+    for i in range(len(levels)):
+        df1 = predictions_df[(predictions_df.ICULOS >= levels[i][0]) & (predictions_df.ICULOS < levels[i][1])]
+        print(f"level {levels[i]}")
+        model_eval(df1.SepsisLabel, df1.predicted, f"test_level_{levels[i]}", title="ICULOS")
+
+def create_features_importance_plot(model,title):
+    data = {"features": model.feature_names_in_, "importance": model.feature_importances_}
+    features_df = pd.DataFrame(data)
+    features_df = features_df.sort_values(by="importance", ascending=False)
+    features_df = features_df.reset_index()
+    fig, ax = plt.subplots()
+    ax.barh(features_df.features, features_df.importance)
+    ax.set_xlabel("importance")
+    for i in range(len(features_df.importance)):
+        plt.text(features_df.importance[i] + 0.01, features_df.index[i], str(round(features_df.importance[i], 3)),
+                 ha='left', va='center')
+
+    ax.set_title(title)
+    ax.invert_yaxis()
+    plt.savefig(title)
+    plt.show()
+
+def HR_analysis(predicted_df):
+    levels = [(-4, -1), (-1, -0.1), (-0.1, 0.1), (0.1, 1), (1, 4)]
+    for i in range(len(levels)):
+        df1 = predicted_df[(predicted_df.HR >= levels[i][0]) & (predicted_df.HR < levels[i][1])]
+        print(f"HR level {levels[i]}")
+        print(f1_score(df1.SepsisLabel, df1.predicted))
+
+def Temp_analysis(predicted_df):
+    levels = [(-4, -1), (-1, -0.1), (-0.1, 0.1), (0.1, 1), (1, 4)]
+    for i in range(len(levels)):
+        df1 = predicted_df[(predicted_df.Temp >= levels[i][0]) & (predicted_df.Temp < levels[i][1])]
+        print(f"Temp level {levels[i]}")
+        print(f1_score(df1.SepsisLabel, df1.predicted))
